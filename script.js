a3cc738fd430e070d631ab93497f18f5a937d2a7
Tom.Code <tomrepar@gmail.com>
2026-02-17 22:34:46 +0100

    `Added view counter to navbar and index.html`

--- script.js
+++ script.js
@@ -43,3 +43,9 @@ fetch("navbar/navbar-root.html")
     });
   });
 // navbar fin
+
+// fetch('https://api.countapi.xyz/hit/tomcaravaca.github.io/visits')
+//   .then(res => res.json())
+//   .then(data => {
+//     document.getElementById('counter').textContent = data.value;
+//   });
